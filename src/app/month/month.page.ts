68fcc41d2da0d7fe40c085995e7c80359bf0bbdd
style <style@cs.technion.ac.il>
2019-03-05 09:34:21 -0500

    calendar support

--- src/app/month/month.page.ts
+++ src/app/month/month.page.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import * as moment from 'moment';
-import {ActivatedRoute} from '@angular/router';
-import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
 import {MaterialService} from '../services/material.service';
 import {MONTHS} from '../app.component';
-import {CalendarComponentOptions} from 'ion2-calendar';
+import {CalendarComponent, CalendarComponentOptions} from 'ion2-calendar';
+import {routerNgProbeToken} from '@angular/router/src/router_module';
 
 @Component({
     selector: 'app-month',
@@ -12,6 +12,8 @@ import {CalendarComponentOptions} from 'ion2-calendar';
     styleUrls: ['./month.page.scss'],
 })
 export class MonthPage implements OnInit {
+    @ViewChild('calendar', {read: CalendarComponent}) calendarRef: CalendarComponent;
+    @ViewChild('content') content;
     private sub: any;
     private month: string;
     monthName: string;
@@ -20,17 +22,31 @@ export class MonthPage implements OnInit {
         pickMode: 'multi',
         from: moment().startOf('year').toDate(),
         to: moment().endOf('year').toDate(),
+        showMonthPicker: false,
+        showToggleButtons: false,
     };
+    dateMulti: string[] = [];
 
 
-    constructor(private route: ActivatedRoute,
+    constructor(private router: Router,
+                private route: ActivatedRoute,
                 private material: MaterialService) {
 
+    }
+
+    onSelect(dateSelected) {
+        // const yOffset = document.getElementById(`${this.month}-${dateSelected.title}`).offsetTop;
+        // this.content.scrollTo(0, yOffset, 1000);
+        console.log(this.dateMulti);
+        this.router.navigateByUrl(`/day/${this.month}-${dateSelected.title}`).then();
     }
 
     ngOnInit() {
         this.sub = this.route.params.subscribe(params => {
             this.month = params['month'];
+            this.optionsMulti.from = moment().set('month', Number(this.month) - 1).startOf('month').toDate();
+            this.optionsMulti.to = moment().set('month', Number(this.month) - 1).endOf('month').toDate();
+            // this.calendarRef.setViewDate(this.optionsMulti.from);
             if (this.month.length < 2) {
                 this.month = '0' + this.month;
             }
