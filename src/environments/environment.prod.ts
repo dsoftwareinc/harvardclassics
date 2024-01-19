dcdc4e0ff257c05970eefb33eac930dfe833b864
DanielM <daniel@moransoftware.ca>
2024-01-19 16:15:59 +0000

    fix:update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyBqFThhFkJF_8FFgzdg1LIsvhixhz30-G8',
-    authDomain: 'harvardclassics365.firebaseapp.com',
-    databaseURL: 'https://harvardclassics365.firebaseio.com',
-    projectId: 'harvardclassics365',
-    storageBucket: 'harvardclassics365.appspot.com',
-    messagingSenderId: '319043773098'
-  },
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyBqFThhFkJF_8FFgzdg1LIsvhixhz30-G8',
+        authDomain: 'harvardclassics365.firebaseapp.com',
+        databaseURL: 'https://harvardclassics365.firebaseio.com',
+        projectId: 'harvardclassics365',
+        storageBucket: 'harvardclassics365.appspot.com',
+        messagingSenderId: '319043773098',
+        appId: "1:319043773098:web:1374a99e677216cd",
+        measurementId: 'G-YW61GDMMKQ',
+    },
 };
-export const APPLICATION_NAME = 'HarvardClassics';
-export const GOOGLE_ANALYTICS_ID = 'UA-64041785-1';
