edc2f88d6d96dd462fc03b4a67f603b8c7d27afd
Daniel M <daniel.maruani@gmail.com>
2026-01-29 15:16:31 +0000

    chore:update to angular 20

--- src/app/notes/notes.page.ts
+++ src/app/notes/notes.page.ts
@@ -1,45 +1,45 @@
-import {Component, OnInit} from '@angular/core';
-import {ReadingDbService} from '../services/readingdb.service';
-import {MaterialService} from '../services/material.service';
+import { Component, OnInit } from '@angular/core';
+import { ReadingDbService } from '../services/readingdb.service';
+import { MaterialService } from '../services/material.service';
 
 @Component({
-    selector: 'app-notes',
-    templateUrl: './notes.page.html',
-    styleUrls: ['./notes.page.scss'],
-    standalone: false
+  selector: 'app-notes',
+  templateUrl: './notes.page.html',
+  styleUrls: ['./notes.page.scss'],
+  standalone: false
 })
 export class NotesPage implements OnInit {
-    data: any;
-    notes = {};
+  data: any;
+  notes: any = {};
 
-    constructor(private db: ReadingDbService,
-                private material: MaterialService) {
-    }
+  constructor(private db: ReadingDbService,
+    private material: MaterialService) {
+  }
 
-    ngOnInit() {
-        this.material.ready().then(json => {
-            this.data = json;
+  ngOnInit() {
+    this.material.ready().then(json => {
+      this.data = json;
+    });
+    console.log(`Adding notes`);
+    this.db.userDocValue().subscribe(data => {
+      const userNotes = data.notes;
+      this.notes = [];
+      userNotes.forEach(note => {
+        const month = note.day.substring(0, 2);
+        const day = note.day.substring(3, 2);
+        const key = '2016-' + note.day;
+        this.notes[key] = this.notes[key] || [];
+        this.notes[key].push({
+          title: +note.day,
+          day: note.day,
+          text: note.text,
         });
-        console.log(`Adding notes`);
-        this.db.userDocValue().subscribe(data => {
-            const userNotes = data.notes;
-            this.notes = [];
-            userNotes.forEach(note => {
-                const month = note.day.substring(0, 2);
-                const day = note.day.substring(3, 2);
-                const key = '2016-' + note.day;
-                this.notes[key] = this.notes[key] || [];
-                this.notes[key].push({
-                    title: +note.day,
-                    day: note.day,
-                    text: note.text,
-                });
-            });
-            console.log(`Added ${userNotes.length} notes`);
-        });
-    }
+      });
+      console.log(`Added ${userNotes.length} notes`);
+    });
+  }
 
-    deleteNote(item: any) {
-        this.db.removeHighlightedText(item.day, item.text);
-    }
+  deleteNote(item: any) {
+    this.db.removeHighlightedText(item.day, item.text);
+  }
 }
